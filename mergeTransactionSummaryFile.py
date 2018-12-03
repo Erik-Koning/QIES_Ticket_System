8908f93be7c7413de6f4f8d88efadfd6eeb7dd7d
PinkViolet <44185081+PinkViolet@users.noreply.github.com>
2018-12-03 14:00:30 -0500

    Update mergeTransactionSummaryFile.py

--- mergeTransactionSummaryFile.py
+++ mergeTransactionSummaryFile.py
@@ -3,27 +3,22 @@ import os.path
 from os import walk
 
 rootDir = os.getcwd()
-testDir = rootDir + '\\merge_testing'
 
 def getTransactionSummaryFileArray():
-    global testDir
-    print(testDir)
     toReturn = []
-    for root, dirs, files in os.walk(testDir):
-        print(root)
-        for f in files:
-            print(f)
-            if "transactionSummary" in f and ".txt" in f:
-                print(f)
-                toReturn.append(f)
+    allFiles = next(os.walk('.'))[2]
+    for f in allFiles:
+        if "transactionSummary" in f and ".txt" in f:
+            toReturn.append(f)
+            
     return toReturn
     
 
 def main():
+    global rootDir
     filenames = getTransactionSummaryFileArray()
-    with open('dailyTransactionSummaryFile.txt', 'w') as outfile:
+    with open(rootDir + '\\dailyTransactionSummaryFile.txt', 'w') as outfile:
         for fname in filenames:
-            print(fname)
             with open(fname) as infile:
                 outfile.write(infile.read())
 
